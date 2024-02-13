b45c3ae4df34f370d713b9f5103af78af6b0eaf4
Anthony Guerra <tx.tony9@gmail.com>
2024-02-12 18:53:28 -0800

    added a routes folder includding a api routes and html routes, and also added a server js to the repo

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const html_routes = require('./routes/html-routes');
+const api_routes = require('./routes/api-routes');
+const PORT = process.env.PORT || 3001;
+const app = express();
+// set the port
