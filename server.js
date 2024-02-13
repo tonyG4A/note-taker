a0f7e5097181c967ce7db4eeaaa70dc2c7246a5f
Anthony Guerra <tx.tony9@gmail.com>
2024-02-13 13:16:54 -0800

    added code to api routes, server.js and html routes to the repo

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const api_routes = require('./routes/api-routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 // set the port
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(api_routes)
+
+app.listen(PORT, () => {
+    console.log(`Sever running on http://localhost:${PORT}`);
+});
