0cf5b3a46eb76af163f8df17d54f3d778b740c18
Anthony Guerra <tx.tony9@gmail.com>
2024-02-25 23:10:51 -0800

    debugged and added new code to the routes and server.js and added a new package json

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const html_routes = require('./routes/html-routes');
-const api_routes = require('./routes/api-routes');
-const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// Initialize the app and create a port
 const app = express();
-// set the port
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: false }));
+// Set up body parsing, static, and route middleware
 app.use(express.json());
-app.use(express.static("public"));
-app.use(html_routes)
-app.use(api_routes)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Sever running on http://localhost:${PORT}`);
-});
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
